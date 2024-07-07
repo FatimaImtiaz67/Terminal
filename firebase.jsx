fc585bb25ff2b57ceea3b571b55d0048cc508bbe
FatimaImtiaz67 <140704322+FatimaImtiaz67@users.noreply.github.com>
2024-07-07 14:01:58 +0500

    Update firebase.jsx

--- firebase.jsx
+++ firebase.jsx
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDZmfFBX_1Q01xAk8frrE-gF_IakU6BSN8",
-  authDomain: "petapp-8752b.firebaseapp.com",
+  apiKey: "A",
+  authDomain: "petm",
   databaseURL: "https://petapp-8752b-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "petapp-8752b",
   storageBucket: "petapp-8752b.appspot.com",
   messagingSenderId: "616591002356",
-  appId: "1:616591002356:web:02168c486b92642b89aca1",
+  appId: "1",
   measurementId: "G-LEL72ZBBW9"
 };
 
